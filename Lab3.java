d715149c47087dffeb1609b88499b5cd986179c6
MohamedYehia20m <77248801+MohamedYehia20m@users.noreply.github.com>
2024-09-04 08:07:45 +0200

    init
    - implement a generic class for complex numbers 
    - implement the basic mathematical operation

---
+++ Lab3.java
@@ -0,0 +1,89 @@
+package EBI;
+
+import java.math.BigDecimal;
+
+public class Lab3 {
+
+    public static void main(String[] args) {
+        ComplexNumber x = new ComplexNumber(new BigDecimal("4.0"), new BigDecimal("4.0"));
+        ComplexNumber y = new ComplexNumber(new BigDecimal("2.0"), new BigDecimal("2.0"));
+
+        ComplexNumber result;
+
+        result = x.subComplex(y);
+        System.out.println("x real after subtraction: " + result.getRe());
+        System.out.println("x img after subtraction: " + result.getIm());
+
+
+
+        result = x.mulComplex(y);
+        System.out.println("x real after multiplication: " + result.getRe());
+        System.out.println("x img after multiplication: " + result.getIm());
+
+        result = x.divComplex(y);
+        System.out.println("x real after division: " + result.getRe());
+        System.out.println("x img after division: " + result.getIm());
+
+        result = x.addComplex(y);
+        System.out.println("x real after division: " + result.getRe());
+        System.out.println("x img after division: " + result.getIm());
+
+
+    }
+
+    public static class ComplexNumber {
+        private BigDecimal Re;
+        private BigDecimal Im;
+
+        // Constructor
+        public ComplexNumber(BigDecimal Re, BigDecimal Im) {
+            this.Re = Re;
+            this.Im = Im;
+        }
+
+        // Getter for Re
+        public BigDecimal getRe() {
+            return Re;
+        }
+
+        // Setter for Re
+        public void setRe(BigDecimal Re) {
+            this.Re = Re;
+        }
+
+        // Getter for Im
+        public BigDecimal getIm() {
+            return Im;
+        }
+
+        // Setter for Im
+        public void setIm(BigDecimal Im) {
+            this.Im = Im;
+        }
+
+        public ComplexNumber addComplex(ComplexNumber num) {
+            BigDecimal newRe = this.Re.add(num.Re);
+            BigDecimal newIm = this.Im.add(num.Im);
+            return new ComplexNumber(newRe, newIm);
+        }
+
+        public ComplexNumber subComplex(ComplexNumber num) {
+            BigDecimal newRe = this.Re.subtract(num.Re);
+            BigDecimal newIm = this.Im.subtract(num.Im);
+            return new ComplexNumber(newRe, newIm);
+        }
+
+        public ComplexNumber mulComplex(ComplexNumber num) {
+            BigDecimal newRe = this.Re.multiply(num.Re).subtract(this.Im.multiply(num.Im));
+            BigDecimal newIm = this.Re.multiply(num.Im).add(this.Im.multiply(num.Re));
+            return new ComplexNumber(newRe, newIm);
+        }
+
+        public ComplexNumber divComplex(ComplexNumber num) {
+            BigDecimal denominator = num.Re.multiply(num.Re).add(num.Im.multiply(num.Im));
+            BigDecimal newRe = (this.Re.multiply(num.Re).add(this.Im.multiply(num.Im))).divide(denominator, BigDecimal.ROUND_HALF_UP);
+            BigDecimal newIm = (this.Im.multiply(num.Re).subtract(this.Re.multiply(num.Im))).divide(denominator, BigDecimal.ROUND_HALF_UP);
+            return new ComplexNumber(newRe, newIm);
+        }
+    }
+}
